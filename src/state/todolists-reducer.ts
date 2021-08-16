9a8a8ce505d3f37d8d4fd71957b062d8842320ec
hp-PC <alexvanden@mail.ru>
2021-08-16 16:11:49 +0300

    task8

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,24 @@
+export type TodolistType = {
+    id: string
+    title: string
+    filter: string
+}
+// type ActionType = {
+//     type: string
+//     [key: string]: any
+// }
+type ActionType = {
+    type: string
+    [key: string]: any
+}
+
+
+export const todolistsReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST' : {
+           return  state.filter(tl => tl.id != action.id)
+        }
+        default:
+            throw new Error('Warning');
+    }
+}
